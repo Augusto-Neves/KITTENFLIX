5cf7fb4991c617c37170490f7339506d3b19f5e9
Augusto-Neves <augusto.agt1995@gmail.com>
2020-07-27 19:55:38 -0300

    Clone da Netflix criado na Imersão React Alura
    Um clone do Layout do Netflix só que com gatinhos.  Projeto ainda em andamento.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,42 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import Menu from '../src/components/Menu/index';
+import dadosIniciais from './data/dados_iniciais.json';
+import BannerMain from './components/BannerMain';
+import Carrousel from './components/Carousel';
+import Footer from './components/Footer'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={ {background: "#141414"}}>
+     <Menu /> 
+
+     <BannerMain
+      videoTitle={dadosIniciais.categorias[0].videos[0].titulo} 
+      url={dadosIniciais.categorias[0].videos[0].url} 
+      videoDescription={"Então essa página foi feita pra você! Fizemos um compilado dos melhores vídeos da internet com os gatos mais fofos e engraçados que você verá."}
+     /> 
+
+     <Carrousel 
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[0]}
+     />
+
+     <Carrousel 
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[1]}
+     />
+
+     <Carrousel 
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[2]}
+     />
+
+     <Carrousel 
+        ignoreFirstVideo
+        category={dadosIniciais.categorias[3]}
+     />
+
+     <Footer />    
     </div>
   );
 }
---
+++ src/components/Footer/index.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { FooterBase } from './styles';
+import Logo from '../../assets/img/Logo.png';
+import Insta from '../../assets/img/Instagram.png'
+import Linkedin from '../../assets/img/Linkedin.png'
+import Github from '../../assets/img/Github.png'
+
+function Footer() {
+  return (
+    <FooterBase>
+      <a href="/">
+        <img className="ImageFooter" src={Logo} alt="Logo Kittenflix" />
+      </a>
+      <p>
+        Orgulhosamente criado por <a href="https://www.instagram.com/_augusto_neves/" target="blank">Augusto Neves</a> durante a
+        {' '}
+        <a href="https://www.alura.com.br/" target='blank'>
+          Imersão React da Alura
+        </a>        
+      </p>
+      <div className="Redes">
+        <a href="https://www.instagram.com/_augusto_neves/" target="blank">
+          <img className="Insta" src={Insta} alt="Instagram" target="blank"/>
+        </a>
+        <a href="https://www.linkedin.com/in/augusto-neves-066b7b1ab/" target='blank'>
+          <img className="Linkedin" src={Linkedin} alt="Linkedin"/>
+        </a>
+        <a href="https://github.com/Augusto-Neves" target="blank" >
+          <img className="GitHub" src={Github} alt="GitHub"/>
+        </a>
+      </div>
+    </FooterBase>
+  );
+}
+
+export default Footer;
