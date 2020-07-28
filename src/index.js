c10794f5f2aa910ee80f12e04c0b85823f2ae10e
Augusto-Neves <augusto.agt1995@gmail.com>
2020-07-28 19:06:01 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -2,10 +2,23 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import { BrowserRouter, Switch, Route} from 'react-router-dom'
+
+function CadastroVideo() {
+  return (
+    <div>
+      Página Cadastro de Vídeos
+    </div>
+  );
+}
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <BrowserRouter>
+    <Switch>
+      <Route path="cadastro/video" component={CadastroVideo} />
+      <Route path="/" component={ App } exact/>        
+    </Switch>
+   
+  </BrowserRouter>,
   document.getElementById('root')
 );
