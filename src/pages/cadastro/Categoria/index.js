737ed3a297cf95170343f940144e11b9e6d3574c
Augusto-Neves <augusto.agt1995@gmail.com>
2020-07-28 22:08:31 -0300

    Update index.js

--- src/pages/cadastro/Categoria/index.js
+++ src/pages/cadastro/Categoria/index.js
@@ -1,29 +1,32 @@
 import React from 'react';
-import PageDefault from '../../../components/pageDefault';
 import { Link } from 'react-router-dom';
+import PageDefault from '../../../components/pageDefault';
 
 function CadastroCategoria() {
-    return (
-      <PageDefault>          
-        <h1>Cadastro de Categoria</h1>
-        
-        <form>
-          <label>
-            Nome da Categoria: 
-            <input type="text"/>
-          </label>
+  return (
+    <PageDefault>
+      <h1>Cadastro de Categoria</h1>
 
-          <button>
-            Cadastrar
-          </button>
-        </form>
+      <form>
 
-       <Link to="/">
-          Ir para a Home 
-        </Link>
-      </PageDefault>
+        <label>
+          Nome da Categoria:
+          <input
+            type="text"
+          />
+        </label>
 
-    );
-  }
+        <button>
+          Cadastrar
+        </button>
+      </form>
+
+
+      <Link to="/">
+        Ir para home
+      </Link>
+    </PageDefault>
+  )
+}
 
 export default CadastroCategoria;
